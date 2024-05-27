397140210dacbc81ebe0c22f74ace10d99d8367a
notageek-guy <manishkumar97430@gmail.com>
2024-05-27 13:46:59 +0530

    setup lipgloss for terminal layout

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,11 +3,54 @@ package main
 import (
 	"fmt"
 
-	"github.com/BigJk/imeji"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/mnsdojo/gofetch/internal/ascii"
+	"github.com/mnsdojo/gofetch/internal/system"
 )
 
-func main() {
+var style = lipgloss.NewStyle().
+	Bold(true).
+	Padding(5).
+	Border(lipgloss.ThickBorder(), true, true, true, true).
+	BorderForeground(lipgloss.Color("228"))
 
-	text, _ := imeji.FileString("./image.png", imeji.WithTrueColor())
-	fmt.Println(text)
+var separatorStyle = lipgloss.NewStyle().
+	Border(lipgloss.NormalBorder(), false, false, true, false).
+	BorderForeground(lipgloss.Color("201")).
+	MarginTop(1).
+	MarginBottom(1)
+
+var headerStyle = lipgloss.NewStyle().
+	Bold(true).
+	Foreground(lipgloss.Color("228"))
+
+var labelStyle = lipgloss.NewStyle().
+	Bold(true).
+	Width(15)
+
+var valueStyle = lipgloss.NewStyle().Bold(true).
+	Width(30)
+
+func main() {
+	info := system.GetSysInfo()
+	char := ascii.GetRandomAsciiArts()
+
+	header := headerStyle.Render("treefetch")
+
+	// Define the labels and their corresponding values
+	labels := []string{"Username:", "OS Release:", "Kernel:", "Default Shell:", "Uptime:", "Memory Usage:"}
+	values := []string{info.UserName, info.OSRelease, info.Kernel, info.DefaultShell, info.Uptime, info.MemoryUsage}
+
+	// Create the output string with aligned labels and values using Lipgloss
+	var output string
+	for i, label := range labels {
+		paddedLabel := labelStyle.Render(label)
+		paddedValue := valueStyle.Render(values[i])
+		output += fmt.Sprintf("%s %s\n", paddedLabel, paddedValue)
+	}
+
+	separator := separatorStyle.Render("──────────────────────────────────────────────────")
+
+	combinedOutput := fmt.Sprintf("%s\n%s\n%s\n%s", header, char, separator, output)
+	fmt.Println(style.Render(combinedOutput))
 }
--- go.mod
+++ go.mod
@@ -6,10 +6,12 @@ require (
 	github.com/BigJk/imeji v0.0.3 // indirect
 	github.com/anthonynsimon/bild v0.13.0 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v0.11.0 // indirect
+	github.com/charmbracelet/x/ansi v0.1.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
-	github.com/mattn/go-runewidth v0.0.14 // indirect
-	github.com/muesli/termenv v0.15.1 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 )
